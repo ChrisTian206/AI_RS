b8b9c0c08b705e51c011116d0c511c231cd50ffa
Wenhao Tian <wtian16@bu.edu>
2024-03-15 14:32:02 -0700

    added intrp

---
+++ client/src/components/Intro.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Container, Row, Col, Card, Button, Image } from 'react-bootstrap'
+
+function Intro() {
+    return (
+        <Container className='mt-2'>
+            <Row className='text-center'>
+                <h1>Vision</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae architecto velit possimus tempore, vero quaerat consequatur dolore, ullam accusantium maxime nisi unde. Nihil autem dignissimos fugiat alias, exercitationem voluptas quasi?</p>
+            </Row>
+            <Row className='text-center'>
+                <h1>Mission</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae architecto velit possimus tempore, vero quaerat consequatur dolore, ullam accusantium maxime nisi unde. Nihil autem dignissimos fugiat alias, exercitationem voluptas quasi?</p>
+            </Row>
+            <Row className='text-center'>
+                <h1>Value</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae architecto velit possimus tempore, vero quaerat consequatur dolore, ullam accusantium maxime nisi unde. Nihil autem dignissimos fugiat alias, exercitationem voluptas quasi?</p>
+            </Row>
+        </Container>
+    )
+}
+
+export default Intro
