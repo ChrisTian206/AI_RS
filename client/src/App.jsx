031ec11598e9971988a192cd513b1acf3668ffe2
Wenhao Tian <wtian16@bu.edu>
2024-03-15 14:30:21 -0700

    added pages routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,11 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import { Container } from 'react-bootstrap'
 import AskAway from './components/AskAway'
+import AzulaaiPage from './pages/AzulaaiPage'
+import ListingPage from './pages/ListingPage'
+import AboutUsPage from './pages/AboutUsPage'
+import InqueryPage from './pages/InqueryPage'
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 
@@ -12,11 +17,13 @@ function App() {
     <BrowserRouter>
       <Header />
       <main className='py-3'>
-        <Container>
-          <Routes>
-            <Route path='/' Component={HomePage} exact />
-          </Routes>
-        </Container>
+        <Routes>
+          <Route path='/' Component={HomePage} exact />
+          <Route path='/askaway' Component={AzulaaiPage} />
+          <Route path='/listing' Component={ListingPage} />
+          <Route path='/about-us' Component={AboutUsPage} />
+          <Route path='/inquery' Component={InqueryPage} />
+        </Routes>
       </main>
       <Footer />
     </BrowserRouter>
