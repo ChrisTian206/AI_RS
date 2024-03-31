ac90519725bf19463daba99ae3fe04d31d2b22e3
Wenhao Tian <wtian16@bu.edu>
2024-03-31 00:07:13 -0700

    moved listings here

--- client/src/pages/ListingPage.jsx
+++ client/src/pages/ListingPage.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Listing from '../components/Listing'
 
 function ListingPage() {
     return (
         <>
             <h1 style={{ textAlign: 'center' }}>Repliers Data Fetch</h1>
+
+            <Listing />
         </>
     )
 }
