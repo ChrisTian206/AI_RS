8993682d3dfe47a5e6fc5902fda88a8fe353bcfd
Wenhao Tian <wtian16@bu.edu>
2024-04-10 14:41:06 -0700

    server config b4 launch

--- server/app.js
+++ server/app.js
@@ -1,23 +1,23 @@
 import express, { json } from 'express';
-import morgan from 'morgan';
+// import morgan from 'morgan';
 import cors from 'cors';
 import dotenv from 'dotenv'
 
 //Init Setup
-import mongoose from 'mongoose';
+// import mongoose from 'mongoose';
 const app = express();
 dotenv.config()
 
-mongoose.connect('mongodb://127.0.0.1:27017/airs')
-    .then(() => { console.log("database is connected!!") })
-    .catch((err) => { console.log(err) })
+// mongoose.connect('mongodb://127.0.0.1:27017/airs')
+//     .then(() => { console.log("database is connected!!") })
+//     .catch((err) => { console.log(err) })
 
-app.use(morgan('dev'))
+// app.use(morgan('dev'))
 app.use(express.json())
 
 //** multi-origin communication */
 const corsOptions = {
-    origin: 'http://localhost:5173', //vite: 5173, React: 3000
+    origin: 'https://ai-rs-git-main-chris-projects-aa6ff9fb.vercel.app/', //For Dev: vite: 5173, React: 3000
 };
 app.use(cors(corsOptions));
 
@@ -27,5 +27,5 @@ app.use('/api/ai', aiRoute)
 
 
 app.listen(8000, () => {
-    console.log('server is live on port 8000')
+    //console.log('server is live on port 8000')
 })
