4e3299fb8970a8f36baaf5e26066c23351e99e3f
Wenhao Tian <wtian16@bu.edu>
2024-04-10 13:32:27 -0700

    disabled listing page fn

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,13 +13,13 @@ import HomePage from './pages/HomePage'
 function App() {
 
   return (
-    <BrowserRouter basename='https://ai-5djc5q99u-chris-projects-aa6ff9fb.vercel.app'>
+    <BrowserRouter>
       <Header />
       <main className='py-3'>
         <Routes>
           <Route path='/' Component={HomePage} exact />
           <Route path='/askaway' Component={AzulaaiPage} />
-          <Route path='/listing' Component={ListingPage} />
+          {/* <Route path='/listing' Component={ListingPage} /> */}
           <Route path='/about-us' Component={AboutUsPage} />
           <Route path='/inquery' Component={InqueryPage} />
           <Route path='/strata' Component={SummarizeStrataPage} />
--- client/src/components/Listing.jsx
+++ client/src/components/Listing.jsx
@@ -3,53 +3,56 @@ import axios from 'axios'
 import { useState, useEffect } from 'react';
 
 function Listing() {
-    const [res, setRes] = useState({})
+    // const [res, setRes] = useState({})
 
-    const options = {
-        method: 'GET',
-        url: 'https://api.repliers.io/listings?city=Vancouver&minBeds=9&minBaths=5&listings=true&operator=AND&sortBy=updatedOnDesc&status=A',
+    // const options = {
+    //     method: 'GET',
+    //     url: '',
 
-        headers: {
-            accept: 'application/json',
-            'REPLIERS-API-KEY': ''
-        },
-    };
+    //     headers: {
+    //         accept: 'application/json',
+    //         'REPLIERS-API-KEY': ''
+    //     },
+    // };
 
-    useEffect(() => {
-        const funCall = async () => {
-            await axios
-                .request(options)
-                .then(function (response) {
-                    //console.log(response.data)
-                    setRes(response.data)
-                    // mistake: if updating a state, put it inside a useEffect OR set a condition like in onClick(), onSubmit()
-                    // original code: setRes(response.data)
-                })
-                .catch(function (error) {
-                    console.error(error);
-                });
-        }
-        funCall()
+    // useEffect(() => {
+    //     const funCall = async () => {
+    //         await axios
+    //             .request(options)
+    //             .then(function (response) {
+    //                 //console.log(response.data)
+    //                 setRes(response.data)
+    //                 // mistake: if updating a state, put it inside a useEffect OR set a condition like in onClick(), onSubmit()
+    //                 // original code: setRes(response.data)
+    //             })
+    //             .catch(function (error) {
+    //                 console.error(error);
+    //             });
+    //     }
+    //     funCall()
 
-    }, [])
+    // }, [])
 
-    console.log('res.listings: ', res.listings)
+    // console.log('res.listings: ', res.listings)
 
 
     return (
-        <div>
-            <h1 style={{ textAlign: 'center' }}>Here is a example of Repliers Call:</h1>
-            <h3 style={{ textAlign: 'center' }}>I want residential properties that has at least 9 bedrooms and 5 bathrooms located in Vancouver</h3>
-            {res.listings ? <h4 style={{
-                textAlign: 'center',
-                marginTop: '10px',
-                borderTop: 'solid black 1px'
-            }}>Here are {res.listings.length} results that fits the description</h4> : ''}
-            {res.listings ? res.listings.map((property, index) => (
-                <p key={index} style={{ textAlign: 'center' }}>{property.address.neighborhood}</p>
-            )) : <h4 style={{ textAlign: 'center', color: 'gray' }}>Loading...</h4>
-            }
-        </div >
+        // <div>
+        //     <h1 style={{ textAlign: 'center' }}>Here is a example of Repliers Call:</h1>
+        //     <h3 style={{ textAlign: 'center' }}>I want residential properties that has at least 9 bedrooms and 5 bathrooms located in Vancouver</h3>
+        //     {res.listings ? <h4 style={{
+        //         textAlign: 'center',
+        //         marginTop: '10px',
+        //         borderTop: 'solid black 1px'
+        //     }}>Here are {res.listings.length} results that fits the description</h4> : ''}
+        //     {res.listings ? res.listings.map((property, index) => (
+        //         <p key={index} style={{ textAlign: 'center' }}>{property.address.neighborhood}</p>
+        //     )) : <h4 style={{ textAlign: 'center', color: 'gray' }}>Loading...</h4>
+        //     }
+        // </div >
+
+        <>
+        </>
     )
 }
 
