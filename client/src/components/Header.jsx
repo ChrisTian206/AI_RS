fc32170e728441078350ad3028a966226c852211
Wenhao Tian <wtian16@bu.edu>
2024-04-10 13:33:30 -0700

    implement a better routing stra

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,47 +5,55 @@ import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Image from 'react-bootstrap/Image';
+import { LinkContainer } from 'react-router-bootstrap'
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from 'react-router-dom';
 
 
 function Header() {
 
     return (
-        <Navbar expand="lg" className="bg-body-tertiary" sticky='top'>
-            <Container fluid>
+        <header>
+            <Navbar expand="lg" className="bg-body-tertiary" sticky='top'>
+                <Container fluid>
 
-                <Navbar.Brand href="/">
-                    <Image src='https://static.wixstatic.com/media/b1603e_c62e5b2318cf43069ed64183b4bada34~mv2.webp' roundedCircle width='50px' height='50px' />
-                    <span className='brand-text'> Azula AI</span>
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="navbarScroll" />
-                <Navbar.Collapse id="navbarScroll">
-                    <Nav
-                        className="me-auto my-2 my-lg-0"
-                        style={{ maxHeight: '100px' }}
-                        navbarScroll
-                    >
-                        <Nav.Link href="/listing">Listings</Nav.Link>
-                        <Nav.Link href="/inquery">Inquery</Nav.Link>
-                        <Nav.Link href="/Strata">Summarize Strata</Nav.Link>
-                        <NavDropdown title="About Us" id="navbarScrollingDropdown">
-                            <NavDropdown.Item href="/about-us">What wo do?</NavDropdown.Item>
-                            <NavDropdown.Item href="/inquery">
-                                Contact
-                            </NavDropdown.Item>
-                            {/* <NavDropdown.Divider /> */}
-                            {/* <NavDropdown.Item href="/career">
+                    <LinkContainer to='/'>
+                        <Navbar.Brand>
+                            <Image src='https://static.wixstatic.com/media/b1603e_c62e5b2318cf43069ed64183b4bada34~mv2.webp' roundedCircle width='50px' height='50px' />
+                            <span className='brand-text'> Azula AI</span>
+                        </Navbar.Brand>
+                    </LinkContainer>
+
+                    <Navbar.Toggle aria-controls="navbarScroll" />
+                    <Navbar.Collapse id="navbarScroll">
+                        <Nav
+                            className="me-auto my-2 my-lg-0"
+                            style={{ maxHeight: '100px' }}
+                            navbarScroll
+                        >
+                            {/* <LinkContainer to='/listing'><Nav.Link >Listings</Nav.Link></LinkContainer> */}
+                            <LinkContainer to='/inquery'><Nav.Link >Inquery</Nav.Link></LinkContainer>
+                            <LinkContainer to='/strata'><Nav.Link >Summarize Strata</Nav.Link></LinkContainer>
+
+                            <NavDropdown title="About Us" id="navbarScrollingDropdown">
+                                <NavDropdown.Item href="/about-us">What wo do?</NavDropdown.Item>
+                                <NavDropdown.Item href="/inquery">
+                                    Contact
+                                </NavDropdown.Item>
+                                {/* <NavDropdown.Divider /> */}
+                                {/* <NavDropdown.Item href="/career">
                                 career
                             </NavDropdown.Item> */}
-                        </NavDropdown>
-                        <Nav.Link href="/askaway">
-                            Ask Azula AI
-                        </Nav.Link>
-                    </Nav>
+                            </NavDropdown>
 
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+
+                            <LinkContainer to='/askaway'><Nav.Link> Ask Azula AI</Nav.Link></LinkContainer>
+                        </Nav>
+
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
     )
 }
 
